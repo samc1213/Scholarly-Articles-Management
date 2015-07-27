77cb86ae81733caa6ee70f474d9e11103ba70e88
Sam Cohen <samc1213@gmail.com>
2015-07-27 16:04:51 -0500

    retried php.ini

--- web/api.php
+++ web/api.php
@@ -1,32 +1,21 @@
 <?php
 	function createuser($username, $password) {
 		$uri = "mongodb://heroku_v7w2qftd:a5h7slci8p0b2p9nt7qe96hmvv@ds027483.mongolab.com:27483/heroku_v7w2qftd";
-		$options = array("connectTimeoutMS" => 30000);
+				
+		$client = new Mongo($uri);
 		
-		$client = new MongoClient($uri, $options);
-		$db = $client->selectDB("heroku_v7w2qftd");
+		$dbname = "heroku_v7w2qftd";
 		
-		$seedData = array(
-		    array(
-		        'decade' => '1970s', 
-		        'artist' => 'Debby Boone',
-		        'song' => 'You Light Up My Life', 
-		        'weeksAtOne' => 10
-		    ),
-		    array(
-		        'decade' => '1980s', 
-		        'artist' => 'Olivia Newton-John',
-		        'song' => 'Physical', 
-		        'weeksAtOne' => 10
-		    ),
-		    array(
-		        'decade' => '1990s', 
-		        'artist' => 'Mariah Carey',
-		        'song' => 'One Sweet Day', 
-		        'weeksAtOne' => 16
-		    ),
+		$db = $client->$dbname;
+		
+		$c_users = $db->users;
+		
+		$user = array(
+			'first_name' => 'MongoDB',
+			'last_name' => 'Fan',
+			'tags' => array('developer','user')
 		);
-		
+				
 		$songs = $db->songs;
 		// To insert a dict, use the insert method.
 		$songs->batchInsert($seedData);
