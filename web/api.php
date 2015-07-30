5ea44ee3d02d256da631695ff3be23f3142065af
Sam Cohen <samc1213@gmail.com>
2015-07-30 13:05:42 -0500

    testing

--- web/api.php
+++ web/api.php
@@ -54,7 +54,9 @@
 		download($_POST['message'], $_POST['data']);
 		if (file_exists('localfile.docx')) {
 			$filestr = file_get_contents('localfile.docx');
+			echo $filestr;
 			$filestr = trim(preg_replace('	', '', $filestr));
+			echo $filestr;
 			file_put_contents('localfile.docx', $filestr);
 			    header("Cache-Control: public");
 			    header("Content-Description: File Transfer");
