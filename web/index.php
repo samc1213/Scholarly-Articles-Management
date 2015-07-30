bd395eb8f7946277e19cb31e67409bad67ac941a
Sam Cohen <samc1213@gmail.com>
2015-07-30 15:38:54 -0500

    messed up readfile

--- web/index.php
+++ web/index.php
@@ -68,7 +68,12 @@
 					if (file_exists('localdoc.docx')) {
 						echo "WOOOT!";
 					}
-						
+					header("Cache-Control: public");
+				    header("Content-Description: File Transfer");
+				    header("Content-Disposition: attachment; filename=file.docx");
+				    header("Content-Type: application/vnd.openxmlformats-officedocument.wordprocessingml.document");
+					header("Content-Length: " . filesize('localdoc.docx'));
+			    	header("Content-Transfer-Encoding: binary");
 					readfile('localdoc.docx');
 						
 				}
