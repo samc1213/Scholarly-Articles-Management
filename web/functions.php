2996e56ffc20f0b36da49c64fd729b8553da0ad2
Sam Cohen <samc1213@gmail.com>
2015-07-30 11:39:40 -0500

    this might help?

--- web/functions.php
+++ web/functions.php
@@ -211,10 +211,9 @@
 				$ch = curl_init();
 				
 				curl_setopt($ch, CURLOPT_URL, $url);
-				curl_setopt($ch, CURLOPT_HEADER, 1);
 				curl_setopt($ch, CURLOPT_TIMEOUT, 50);
 				curl_setopt($ch, CURLOPT_FILE, $fp); // write curl response to file
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
 				curl_exec($ch);			
 			
