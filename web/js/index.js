deb37d40e1c959f4ca301c2985e8838e2fca38f2
Sam Cohen <samc1213@gmail.com>
2015-08-05 15:06:50 -0500

    js isnt loading in DOM

--- web/js/index.js
+++ web/js/index.js
@@ -118,14 +118,7 @@ $(document).ready( function () {
 				grant.awardperiod2 = $(this).find(".todate").text();
 				grant.firstname = $(document).find("#firstname").text();
 				grant.lastname = $(document).find("#lastname").text();
-				$mname = $(document).find("#middlename");
-				console.log($mname);
-				if ($mname.is(":empty")){
-					grant.middlename = '';
-				}
-				else {
-					grant.middlename = $mname.text();
-				}
+				grant.middlename = $(document).find("#middlename").text();
 				console.log("mname" + grant.middlename);
 				grant.location = $(this).find(".locationval").text();
 				data.push(grant);			
