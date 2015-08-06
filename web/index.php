3ec594b358b3c9f792c6b056368a55ca868f1a35
Sam Cohen <samc1213@gmail.com>
2015-08-06 11:47:36 -0500

    tryna center that darn delete btn

--- web/index.php
+++ web/index.php
@@ -186,7 +186,7 @@
 									echo $count;
 									echo '"><i class="fa fa-pencil-square-o"';
 									echo '"></i></button></td>';
-									echo '<td><button class="btn btn-default deletebtn" style="padding: 0;" id="deletebtn';
+									echo '<td class="deletetd"><button class="btn btn-default deletebtn" style="padding: 0;" id="deletebtn';
 									echo $count;
 									echo '"><i class="fa fa-trash-o"';
 									echo '"></i></button></td>';
