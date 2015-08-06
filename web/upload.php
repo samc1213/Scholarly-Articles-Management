ce128ba1554b050b4d66466d80bf892a9a760efb
Sam Cohen <samc1213@gmail.com>
2015-08-06 15:25:46 -0500

    trying that ajax upload good stuff

--- web/upload.php
+++ web/upload.php
@@ -1,9 +1,27 @@
 <?php
+	require ('/app/vendor/autoload.php');
+	
+	
     if ( 0 < $_FILES['file']['error'] ) {
         echo 'Error: ' . $_FILES['file']['error'] . '<br>';
     }
     else {
         move_uploaded_file($_FILES['file']['tmp_name'], 'uploads/' . $_FILES['file']['name']);
 		echo file_get_contents('uploads/' . $_FILES['file']['name']);
+		
+		$config = array('key' => getenv('AWS_ACCESS_KEY_ID'), 'secret' => getenv('AWS_SECRET_ACCESS_KEY'));
+		$s3 = Aws\S3\S3Client::factory();
+		
+		$result = $s3->putObject(array(
+	    'Bucket'       => 'cpgrantsuploads',
+	    'Key'          => 'keykey',
+	    'SourceFile'   => 'uploads/' . $_FILES['file']['name'],
+	    'ContentType'  => 'text/plain',
+	    'ACL'          => 'public-read',
+	    'StorageClass' => 'REDUCED_REDUNDANCY',
+	    'Body'			=> 'hi world!',
+		));
+		
+		
     }
 ?>
