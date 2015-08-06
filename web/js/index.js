78b2ae6d3569d45d29a4f93f0700297174b74bc4
Sam Cohen <samc1213@gmail.com>
2015-08-06 16:19:31 -0500

    trying that ajax upload good stuff

--- web/js/index.js
+++ web/js/index.js
@@ -11,24 +11,23 @@ $(document).ready( function () {
 	}
 	
 	$('#filesubmit').submit( function() {
-		var form = $("#filesubmit");
-		console.log(form);
+		var form = document.getElementById('filesubmit');
 		var form_data = new FormData(form);
 		console.log(form_data);
 	    // var form_data = new FormData();                  
 	    // form_data.append('file', file_data);
-	    // $.ajax({
-	                // url: 'upload.php', // point to server-side PHP script 
-	                // dataType: 'text',  // what to expect back from the PHP script, if anything
-	                // cache: false,
-	                // contentType: false,
-	                // processData: false,
-	                // data: form_data,                         
-	                // type: 'post',
-	                // success: function(php_script_response){
-	                    // console.log(php_script_response); // display response from the PHP script, if any
-	                // }
-	     // });
+	    $.ajax({
+	                url: 'upload.php', // point to server-side PHP script 
+	                dataType: 'text',  // what to expect back from the PHP script, if anything
+	                cache: false,
+	                contentType: false,
+	                processData: false,
+	                data: form_data,                         
+	                type: 'post',
+	                success: function(php_script_response){
+	                    console.log(php_script_response); // display response from the PHP script, if any
+	                }
+	     });
 	});
 
 	
--- web/upload.php
+++ web/upload.php
@@ -1,34 +1,35 @@
 <?php
-	require ('/app/vendor/autoload.php');
-	
-	session_start();
-	
-	$uploadOk = 1;
-	
-    if ( 0 < $_FILES['file']['error'] ) {
-        echo 'Error: ' . $_FILES['file']['error'] . '<br>';
-    }
-    else {
-    	if ($_FILES["file"]["size"] > 2000000) {
-		    echo "Sorry, your file is too large. Limit to 200kb";
-		    $uploadOk = 0;
-		}
-		
-		if ($uploadOk == 1) {
-	        move_uploaded_file($_FILES['file']['tmp_name'], 'uploads/' . $_FILES['file']['name']);
-			echo file_get_contents('uploads/' . $_FILES['file']['name']);
-			
-			$config = array('key' => getenv('AWS_ACCESS_KEY_ID'), 'secret' => getenv('AWS_SECRET_ACCESS_KEY'));
-			$s3 = Aws\S3\S3Client::factory();
-			
-			$result = $s3->putObject(array(
-		    'Bucket'       => 'cpgrantsuploads',
-		    'Key'          => $_SESSION['username'].'/'.$_FILES['file']['name'],
-		    'SourceFile'   => 'uploads/' . $_FILES['file']['name'],
-		    'ContentType'  => 'text/plain',
-		    'ACL'          => 'public-read',
-		    'StorageClass' => 'REDUCED_REDUNDANCY',
-			));			
-		}
-    }
+	echo var_dump($_FILES);
+	// require ('/app/vendor/autoload.php');
+// 	
+	// session_start();
+// 	
+	// $uploadOk = 1;
+// 	
+    // if ( 0 < $_FILES['file']['error'] ) {
+        // echo 'Error: ' . $_FILES['file']['error'] . '<br>';
+    // }
+    // else {
+    	// if ($_FILES["file"]["size"] > 2000000) {
+		    // echo "Sorry, your file is too large. Limit to 200kb";
+		    // $uploadOk = 0;
+		// }
+// 		
+		// if ($uploadOk == 1) {
+	        // move_uploaded_file($_FILES['file']['tmp_name'], 'uploads/' . $_FILES['file']['name']);
+			// echo file_get_contents('uploads/' . $_FILES['file']['name']);
+// 			
+			// $config = array('key' => getenv('AWS_ACCESS_KEY_ID'), 'secret' => getenv('AWS_SECRET_ACCESS_KEY'));
+			// $s3 = Aws\S3\S3Client::factory();
+// 			
+			// $result = $s3->putObject(array(
+		    // 'Bucket'       => 'cpgrantsuploads',
+		    // 'Key'          => $_SESSION['username'].'/'.$_FILES['file']['name'],
+		    // 'SourceFile'   => 'uploads/' . $_FILES['file']['name'],
+		    // 'ContentType'  => 'text/plain',
+		    // 'ACL'          => 'public-read',
+		    // 'StorageClass' => 'REDUCED_REDUNDANCY',
+			// ));			
+		// }
+    // }
 ?>
