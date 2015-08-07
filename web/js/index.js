e1020c73a3fd35b356c51bc012ea5f0780653feb
Sam Cohen <samc1213@gmail.com>
2015-08-07 16:50:55 -0500

    testing

--- web/js/index.js
+++ web/js/index.js
@@ -18,11 +18,16 @@ $(document).ready( function () {
 	    $.ajax({
 	                url: 'upload.php', // point to server-side PHP script 
 	                dataType: 'text',  // what to expect back from the PHP script, if anything
+	                cache: false,
 	                contentType: false,
+	                processData: false,
 	                data: form_data,                         
 	                type: 'post',
 	                success: function(php_script_response){
 	                    console.log(php_script_response); // display response from the PHP script, if any
+	                }
+	                error: function (e) {
+	                	console.log(e);
 	                }
 	     });
 	});
