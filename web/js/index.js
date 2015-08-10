19dcb5edb74dbdc7f25a87c95535afb1c38df26d
Sam Cohen <samc1213@gmail.com>
2015-08-10 12:44:03 -0500

    nesting some ajax. woah. not working yet

--- web/js/index.js
+++ web/js/index.js
@@ -164,7 +164,7 @@ $(document).ready( function () {
 							data:
 							{
 								type: "getfiles",
-								grantname: grantname,
+								grantname: gfilename,
 							},
 							success: function(data)
 							{
