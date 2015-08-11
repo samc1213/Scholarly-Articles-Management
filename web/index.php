694b6acde915b6b0f624af656bc9785f6e8798f2
Sam Cohen <samc1213@gmail.com>
2015-08-11 11:12:51 -0500

    got rid of user icon

--- web/index.php
+++ web/index.php
@@ -42,7 +42,6 @@
 	        	<?php
 	        		session_start();
 					if (isset($_SESSION['username'])) {
-						echo '<i class="fa fa-user"></i>';
 						echo '<button id="logoutheaderbutton" class="logbutton">Logout</button>';		
 					}
 					else {
