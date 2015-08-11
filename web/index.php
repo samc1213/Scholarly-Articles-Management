48f192aced14dadde5d04376307be812ebfc7e35
Sam Cohen <samc1213@gmail.com>
2015-08-11 11:02:28 -0500

    minor problem. should be fixed

--- web/index.php
+++ web/index.php
@@ -211,7 +211,7 @@
 									echo '<td class="status">'.$status.'</span></td>';
 									echo '<td class="awardnumber" style="display:none;">'.$awardnumber.'</td>';
 									echo '<td class="grantagency">'.$agency.'</td>';
-									echo '<td class="amount"><span class = "amountdol">'.$amountdol.'</span><span class="amountnum">'.$amount.'</span>'.$amountlabel.'<span class="totamountdol">'.$totamaountdol.'</span><span class="totamountnum">'.$totamount.'</span>'.$totamountlabel.'</td>';
+									echo '<td class="amount"><span class = "amountdol">'.$amountdol.'</span><span class="amountnum">'.$amount.'</span>'.$amountlabel.'<span class="totamountdol">'.$totamountdol.'</span><span class="totamountnum">'.$totamount.'</span>'.$totamountlabel.'</td>';
 									echo '<td class="piamount"><span class="piamountdol">'.$piamountdol.'</span><span class="piamountnum">'.$piamount.'</span>'.$piamountlabel.'<span class="totpiamountdol">'.$totpiamountdol.'</span><span class="totpiamountnum">'.$totpiamount.'</span>'.$totpiamountlabel.'</td>';
 									echo '<td class="pmonths"><span class="apmonthnum">'.$apmonths.'</span>'.$alabel.'<span class="cpmonthnum">'.$cpmonths.'</span>'.$clabel.'<span class="spmonthnum">'.$spmonths.'</span>'.$slabel.'</td>';
 									echo '<td class="from"><span class="fromdate">'.$fromdate.'</span></td>';
