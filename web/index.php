dd48ab187378cfb6ef325c19a26e5f79b8eb8462
Sam Cohen <samc1213@gmail.com>
2015-08-13 15:47:13 -0500

    done for the day

--- web/index.php
+++ web/index.php
@@ -4,7 +4,7 @@
     	<link rel="icon" href="favicon.ico" type="image/x-icon"/>	
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Grants</title>
+        <title>Scholarly Articles Management</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
