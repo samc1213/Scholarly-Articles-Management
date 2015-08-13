75b9d010c0f738a4b948aca21f4ed92c3d3a46c0
Sam Cohen <samc1213@gmail.com>
2015-08-13 15:38:09 -0500

    layout changes. looking good

--- web/index.php
+++ web/index.php
@@ -71,7 +71,7 @@
 					echo "'s Grants</h2>";						
 				}
 				else {
-					header('Location: http://'.$_SERVER[HTTP_HOST].$_SERVER[REQUEST_URI].'/login.html');
+					header('Location: http://'.$_SERVER[HTTP_HOST].$_SERVER[REQUEST_URI].'login.html');
 				}
 				
 				?>
