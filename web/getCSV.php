ceb1e64c7454f0ce75ca5f6cb7e79fc831205d15
Sam Cohen <samc1213@gmail.com>
2015-08-17 11:09:27 -0500

    testing

--- web/getCSV.php
+++ web/getCSV.php
@@ -1,6 +1,7 @@
 <?php
 	header("Content-type: application/octet-stream");
 	header("Content-Disposition: attachment; filename=\"my-data.csv\"");
+	header('Refresh: 0; url=index.php');
 	$data=stripcslashes($_REQUEST['csv_text']);
 	echo $data; 
 ?>
